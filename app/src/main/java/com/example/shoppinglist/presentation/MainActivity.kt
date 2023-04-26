3cdb9545aa9916c2c31c49c06537663d6d412009
Sergiy Povzhyk <sergiypovzhyk@gmail.com>
2023-04-26 20:27:11 +0300

    create ShopListAdapter and bugfix

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -3,24 +3,29 @@ package com.example.shoppinglist.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: ShopListAdapter
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
-
+            adapter.shopList = it
         }
-        viewModel.getShopList()
 
+    }
 
-
+    private fun setupRecyclerView(){
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
     }
 }
